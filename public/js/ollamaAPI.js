306e4c80daffce40913faff5969971ab56053f1d
Mattias <106891644+Neniuk@users.noreply.github.com>
2023-11-11 23:31:38 +0200

    .

--- public/js/ollamaAPI.js
+++ public/js/ollamaAPI.js
@@ -1,10 +1,11 @@
 const postOllama = async (model, prompt, stream) => {
 	const req = "http://localhost:11434/api/generate";
 
+	console.log("Starting ollama request...");
 	const body = {
 		model: model,
 		prompt: prompt,
-		stream: false,
+		// stream: false,
 	};
 
 	console.log("req: ", req);
@@ -15,6 +16,22 @@ const postOllama = async (model, prompt, stream) => {
 		},
 		body: JSON.stringify(body),
 	});
+	const reader = res.body.getReader();
+	const decoder = new TextDecoder();
+
+	while (true) {
+		const { done, value } = await reader.read();
+		if (done) {
+			console.log("done");
+			return;
+		}
+		console.log("value: " + value);
+
+		// Decode the chunk and append it to the DOM
+		const chunkText = decoder.decode(value);
+		console.log("chunkText: " + chunkText);
+	}
+
 	const data = await res.json();
 
 	return data;
