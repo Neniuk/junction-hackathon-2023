1c159219d1ba05e85acef6e57bb1dfd3fcedbb2a
AndreiToma123 <118735556+AndreiToma123@users.noreply.github.com>
2023-11-11 17:23:45 +0200

    .

--- public/js/basic.js
+++ public/js/basic.js
@@ -1,6 +1,8 @@
 import { getElectricityPrices } from "./electricityPriceAPI.js";
 import { getMaterialData } from "./materialData.js";
 import { postOllama } from "./ollamaAPI.js";
+import Chart from "/chart.js/auto";
+
 
 // import { post } from "../../app.js";
 
@@ -32,11 +34,55 @@ async function initializeCode() {
 	}
 
 	// endTime, startTime, value, variableID
-	const data = await postOllama(
-		"llama2",
-		"You are Mario",
-		"What is your job?",
-		false
-	);
-	console.log("data: ", data);
+	// const data = await postOllama(
+	// 	"llama2",
+	// 	"You are Mario",
+	// 	"What is your job?",
+	// 	false
+	// );
+	// console.log("data: ", data);
+	console.log("materialData: ", await materialData.materials);
+
+	
+new Chart(
+	document.getElementById('myChart'),
+		{
+		  type: 'line',
+		  data: {
+			labels: data.map(entry => entry.timeData), 
+			datasets: [
+			  {
+				label: 'Material Price Data',
+				data: data.map(entry => entry.priceData),
+				fill: false,
+				borderColor: 'rgba(75, 192, 192, 1)',
+				borderWidth: 2,
+				pointBackgroundColor: 'rgba(75, 192, 192, 1)',
+				pointRadius: 5,
+				pointHoverRadius: 8,
+			  }
+			]
+		  },
+		  options: {
+			scales: {
+			  x: {
+				type: 'linear',
+				position: 'bottom',
+				title: {
+				  display: true,
+				  text: 'Time'
+				}
+			  },
+			  y: {
+				type: 'linear',
+				title: {
+				  display: true,
+				  text: 'Price'
+				}
+			  }
+			}
+		  }
+		}
+	  );
+	  
 }
