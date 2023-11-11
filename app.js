b64e24f8af2a5b20e643fbc9a3e573889e83950d
Mattias <106891644+Neniuk@users.noreply.github.com>
2023-11-11 02:41:03 +0200

    functioning yle article web scraper api, note, may need to limit number of articles pulled.

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var http = require('http');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var http = require("http");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var yleRouter = require("./routes/getYleArticles");
 
 var app = express();
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/yle", yleRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-// render the error page
-res.status(err.status || 500);
-res.send('error');//this or res.status(err.status || 500).send('error')
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.send("error"); //this or res.status(err.status || 500).send('error')
 });
 
 module.exports = app;
---
+++ routes/getYleArticles.js
@@ -0,0 +1,114 @@
+var express = require('express');
+var router = express.Router();
+var puppeteer = require("puppeteer");
+
+const englishEnergyArticles = "https://yle.fi/news/18-40146";
+const finnishEnergyArticles = "https://yle.fi/uutiset/18-796";
+// const customSearch = "https://haku.yle.fi/?query=" + query + "&type=article"
+
+async function startBrowser() {
+    let browser;
+    try {
+        console.log("Opening the browser......");
+        browser = await puppeteer.launch({
+            headless: false,
+            args: ["--disable-setuid-sandbox"],
+            ignoreHTTPSErrors: true,
+        });
+    } catch (err) {
+        console.log("Could not create a browser instance => : ", err);
+    }
+    return browser;
+}
+
+const getArticles = async (browser, articleSearchLink1, articleSearchLink2) => {
+    // Start a Puppeteer session with:
+    // const browser = await startBrowser();
+
+    // Open a new page
+    let page = await browser.newPage();
+
+    // Navigate to the selected page
+    console.log(`Navigating to ${articleSearchLink1}...`);
+    await page.goto(articleSearchLink1, {
+        waitUntil: "domcontentloaded",
+    });
+
+    const articles = await page.$('#yle__contentAnchor');
+    const links = await articles.$$('.underlay-link');
+
+    const propertyJsHandles = await Promise.all(
+        links.map(handle => handle.getProperty('href'))
+    );
+    const hrefs = await Promise.all(
+        propertyJsHandles.map(handle => handle.jsonValue())
+    );
+
+    let page2 = await browser.newPage();
+    console.log(`Navigating to ${articleSearchLink1}...`);
+    await page2.goto(articleSearchLink2, {
+        waitUntil: "domcontentloaded",
+    });
+
+
+    const articles2 = await page2.$('#yle__contentAnchor');
+    const links2 = await articles2.$$('.underlay-link');
+
+    const propertyJsHandles2 = await Promise.all(
+        links2.map(handle => handle.getProperty('href'))
+    );
+    const hrefs2 = await Promise.all(
+        propertyJsHandles2.map(handle => handle.jsonValue())
+    );
+    // console.log(hrefs);
+
+    const articleLinks = hrefs.concat(hrefs2);
+
+    return articleLinks
+
+    // await browser.close();
+};
+
+const getArticleContent = async (browser, articleLink) => {
+    // Start a Puppeteer session with:
+    // const browser = await startBrowser();
+
+    // Open a new page
+    let page = await browser.newPage();
+
+    // Navigate to the selected page
+    console.log(`Navigating to ${articleLink}...`);
+    await page.goto(articleLink, {
+        waitUntil: "domcontentloaded",
+    });
+
+    const articleContent = await page.$('.yle__article__content');
+    const paragraphs = await articleContent.$$('p');
+
+    const propertyJsHandles = await Promise.all(
+        paragraphs.map(handle => handle.getProperty('textContent'))
+    );
+    const texts = await Promise.all(
+        propertyJsHandles.map(handle => handle.jsonValue())
+    );
+
+    return texts
+
+    // await browser.close();
+};
+
+router.get('/', async function(req, res, next) {
+    const browser = await startBrowser();
+    message = {
+        articles: [],
+    }
+    const articles = await getArticles(browser, englishEnergyArticles, finnishEnergyArticles);
+    console.log(articles);
+    for (let i = 0; i < 2; i++) {
+        const articleContent = await getArticleContent(browser, articles[i]);
+        message.articles.push(articleContent);
+    }
+    res.json(message);
+});
+
+module.exports = router;
